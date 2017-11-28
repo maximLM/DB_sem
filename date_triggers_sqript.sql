dd0f5ce4e071728769a4ffc624d9462a9246ff4a
Maxim <lesssmeaning@gmail.com>
2017-11-28 13:07:00 +0300

    create db script and some triggers

---
+++ date_triggers_sqript.sql
@@ -0,0 +1,27 @@
+﻿CREATE OR REPLACE FUNCTION set_current_date() RETURNS trigger AS 
+$$
+    BEGIN
+	IF NEW.created is NULL THEN
+		NEW.created = current_date;
+	END IF;
+	RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER created_date BEFORE INSERT ON users
+FOR EACH ROW EXECUTE PROCEDURE set_current_date();
+
+CREATE TRIGGER created_date BEFORE INSERT ON posts
+FOR EACH ROW EXECUTE PROCEDURE set_current_date();
+
+CREATE TRIGGER created_date BEFORE INSERT ON tags
+FOR EACH ROW EXECUTE PROCEDURE set_current_date();
+
+CREATE TRIGGER created_date BEFORE INSERT ON comments
+FOR EACH ROW EXECUTE PROCEDURE set_current_date();
+
+CREATE TRIGGER created_date BEFORE INSERT ON post_likes
+FOR EACH ROW EXECUTE PROCEDURE set_current_date();
+
+CREATE TRIGGER created_date BEFORE INSERT ON photo
+FOR EACH ROW EXECUTE PROCEDURE set_current_date();
---
+++ like_trigger.sql
@@ -0,0 +1,13 @@
+﻿CREATE OR REPLACE FUNCTION like_trigger() RETURNS trigger AS 
+$$
+    BEGIN
+	UPDATE posts SET likes = (
+	(SELECT likes FROM posts WHERE id = NEW.post_id) + 1
+	)
+	WHERE id = NEW.post_id;
+	RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+DROP TRIGGER IF EXISTS like_trigger ON post_likes;
+CREATE TRIGGER like_trigger BEFORE INSERT ON post_likes
+FOR EACH ROW EXECUTE PROCEDURE like_trigger();
