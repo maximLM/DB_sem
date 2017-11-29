2a867f63dfe88d13804bbec408175d0f9c08e404
Maxim <lesssmeaning@gmail.com>
2017-11-30 01:21:18 +0300

    corrected fill func

--- denormalized_table.sql
+++ denormalized_table.sql
@@ -9,6 +9,7 @@ CREATE OR REPLACE FUNCTION fill_denormalized_table()
 	RETURNS TABLE(a INTEGER,b TEXT,c INTEGER,d INTEGER,e DATE) AS 
 $$
 BEGIN
+	TRUNCATE TABLE posts_with_tags;
 	INSERT INTO posts_with_tags 
 	SELECT PT.post_id, PT.text, PT.tag_id, PT.user_id, PT.created FROM 
 	(posts JOIN post_tag ON posts.id = post_tag.tag_id) AS PT
