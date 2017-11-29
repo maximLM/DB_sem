2f8512e20598bebb65553b75e00638a52084c945
Maxim <lesssmeaning@gmail.com>
2017-11-30 02:06:28 +0300

    added indexes

---
+++ indexes.sql
@@ -0,0 +1,5 @@
+﻿--DROP INDEX kek;
+CREATE INDEX posts_user_id ON posts ((user_id));
+CREATE INDEX comments_post_id ON comments (post_id);
+CREATE INDEX tags_txt ON tags(text);
+CREATE INDEX post_tags ON post_tag(post_id);
